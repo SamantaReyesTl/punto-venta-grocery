379dfa203128134aba6dc33bfdfab2e0318aaee4
samanta reyes <samanta.reyestl@alumno.buap.mx>
2023-08-15 18:22:48 -0600

    First commit

---
+++ lib/Screens/Login/login.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'components/body.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
